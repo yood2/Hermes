7b091a769a74b07cbbcad5240675de51c401b642
yood2 <yood2@proton.me>
2024-01-06 05:16:33 -0800

    Implemented future_value_of_stream() and created unit tests for method

--- interest_module/__init__.py
+++ interest_module/__init__.py
@@ -6,6 +6,7 @@ r = interest rate
 c = cash flow
 n = number of periods
 '''
+
 # REQUIRES: pv > 0, r >= 0
 # EFFECT: calculates future value of a cash flow given an interest rate and number of periods 
 def compound(pv, r, n):
@@ -23,15 +24,31 @@ def calculate_effective_annual_rate(r, n, c):
 
 # REQUIRES: collection of double called cash_flows, r > 0
 # EFFECT: use the discount method recursively to find the present value of cash flow streams
-def present_value(cash_flows, r, n=0):
+def present_value_of_stream(cash_flows, r, n=0):
     # Returns 0 if there are no more cash flows
     if n >= len(cash_flows):
         return 0
     
     # Calculates PV for current period, recursively calls method with increased period
     pv_current = discount(cash_flows[n], r, n)
-    pv_remaining = present_value(cash_flows, r, n+1)
+    pv_remaining = present_value_of_stream(cash_flows, r, n+1)
     
     # Add all the periods and return total_pv
     total_pv = pv_current + pv_remaining
     return total_pv
+
+# REQUIRES: collection of double called cash_flows, r > 0
+# EFFECT: iterate through list of cash_flows and compound each cash flow to get future value
+def future_value_of_stream(cash_flows, r):
+    fv_total = 0
+    n = len(cash_flows)
+
+    for i in range(n):
+        # number of periods from current cash flow to end
+        periods = n - i - 1
+        
+        # Calculate future value of each cash flow
+        fv = compound(cash_flows[i], r, periods)
+        fv_total += fv
+    
+    return fv_total
--- test.py
+++ test.py
@@ -11,14 +11,14 @@ class TestInterestModule(unittest.TestCase):
         num_periods = 5
         result = round(compound(initial_value, interest_rate, num_periods),2)
         expected_result = 1276.28
-        self.assertEquals(result, expected_result)
+        self.assertEqual(result, expected_result)
     def test_compound_negative_values(self):
         initial_value = -500
         interest_rate = 0.1
         num_periods = 3
         result = round(compound(initial_value, interest_rate, num_periods),2)
         exepcted_result = -665.50
-        self.assertEquals(result, exepcted_result)
+        self.assertEqual(result, exepcted_result)
 
     # tests for discount(fv, r, n)
     def test_discount_positive_values(self):
@@ -27,14 +27,14 @@ class TestInterestModule(unittest.TestCase):
         num_periods = 5
         result = round(discount(initial_value, interest_rate, num_periods),2)
         expected_result = 1000.00
-        self.assertEquals(result, expected_result)
+        self.assertEqual(result, expected_result)
     def test_discount_negative_values(self):
         initial_value = -665.50
         interest_rate = 0.1
         num_periods = 3
         result = round(discount(initial_value, interest_rate, num_periods),2)
         expected_result = -500.00
-        self.assertEquals(result, expected_result)
+        self.assertEqual(result, expected_result)
 
     # tests for calculate_effective_annual_rate(r, n, c)
     def test_calculate_effective_annual_rate(self):
@@ -43,15 +43,23 @@ class TestInterestModule(unittest.TestCase):
         cash_flows = 12
         result = round(calculate_effective_annual_rate(apr, periods, cash_flows),4)
         expected_result = 0.0512
-        self.assertEquals(result, expected_result)
+        self.assertEqual(result, expected_result)
 
     # tests for present_value(cash_flows, r, n=0)
     def test_present_value(self):
         cash_flows = [100,200,150]
         discount_rate = 0.05
-        result = round(present_value(cash_flows, discount_rate),2)
+        result = round(present_value_of_stream(cash_flows, discount_rate),2)
         expected_result = 426.53
-        self.assertEquals(result, expected_result)
+        self.assertEqual(result, expected_result)
+
+    # tests for future_value(cash_flows, r, n=0)
+    def test_present_value(self):
+        cash_flows = [100,200,150]
+        interest_rate = 0.05
+        result = round(future_value_of_stream(cash_flows, interest_rate),2)
+        expected_result = 470.25
+        self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
