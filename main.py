8608f2ca5a161f474e56d5dae628c3286351492e
Daniel Yoo <yood2@Daniels-MacBook-Pro.local>
2024-01-06 02:34:11 -0800

    Seperate interest methods into interest_module, created main.py to run main function

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-# REQUIRES: initialValue > 0, interestRate > 0, numPeriods > 0
-# EFFECT: calculates future value of compounding cash flows
-def compound(initial_value, interest_rate, num_periods):
-    return initial_value * (1+interest_rate)**num_periods
+from interest_module import *
 
-# REQUIRES: initialValue > 0, interestRate > 0, numPeriods > 0
-# EFFECT: calculates present value of future cash flow
-def discount(future_value, interest_rate, num_periods):
-    return future_value * 1/(1+interest_rate)**num_periods
+def main():
+    a = 100
+    b = 0.05
+    c = 10
 
-# REQUIRES: apr > 0, periods > 0, cash_flows > 0
-# EFFECT: calculates EAR using APR, number of periods, and cash flows per year
-def calculate_effective_annual_rate(apr, periods, cash_flows):
-    return (1 + apr/periods)**(periods/cash_flows)
+    d = compound(a,b,c)
+    print(d)
+    print(discount(d,b,c))
+    print(calculate_effective_annual_rate(0.05,12,12))
 
-
-#print(compound(a,b,c))
-#print(discount(compound(a,b,c),b,c))
+if __name__ == '__main__':
+    main()
