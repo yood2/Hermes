751e8d4d6cda6627c6d0f72fe1ef0466407cb6f7
yood2 <yood2@proton.me>
2024-01-10 16:58:02 -0800

    Redid main method with yfinance and pandas packages

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from assetAnalysis import *
+import pandas as pd
 import yfinance as yf
-from datetime import datetime
 
 def main():
-    # CREATE TICKER INSTANCE FOR AMAZON
-    amzn = yf.Ticker("AMZN")
-
-    # GET TODAYS DATE AND CONVERT IT TO A STRING WITH YYYY-MM-DD FORMAT (YFINANCE EXPECTS THAT FORMAT)
-    end_date = datetime.now().strftime('%Y-%m-%d')
-    amzn_hist = amzn.history(start='2022-01-01',end=end_date)
-    print(amzn_hist)
+    symbol = 'MSFT'
+    data = yf.Ticker(symbol)
+    ts = data.history(start='2021-01-01', end='2023-12-31')
+    df = pd.DataFrame(ts)
+    df
 
 if __name__ == '__main__':
     main()
